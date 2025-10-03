c0b18744449adf5383c7577bb10ea498146062a2
Vandavasi Swaroop <v.swaroop@andersenlab.com>
2025-10-03 10:12:30 +0530

    added console to get app url

--- src/main.ts
+++ src/main.ts
@@ -21,5 +21,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3002);
+  console.log('Application is Deployed AT ', await app.getUrl());
 }
 bootstrap();
